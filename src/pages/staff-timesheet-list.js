a7d3919fabe600c35d52b52409b2c7d930c49aae
ifiokLab <ifiokudoh77@gmail.com>
2024-06-18 21:31:05 +0100

    trdewas

--- src/pages/staff-timesheet-list.js
+++ src/pages/staff-timesheet-list.js
@@ -211,7 +211,7 @@ const StaffsTimesheetList = ()=>{
                        <div className='organization-header'>
                         <div className='box1'>
                             <div className='box1-logo'>
-                                <img src={`${organization.logo}`} alt = {organization.name}/>
+                                <img src={`${apiUrl}${organization.logo}`} alt = {organization.name}/>
                                 <div className='org-name'>{organization.name}</div>
                             </div>
                         </div>
