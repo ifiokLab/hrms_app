94e16477c1de55a833b363a8731e8b2edb4eca38
ifiokLab <ifiokudoh77@gmail.com>
2024-06-29 12:06:22 +0100

    POUYTTR

--- src/components/api-url.js
+++ src/components/api-url.js
@@ -1,4 +1,5 @@
 //const apiUrl = 'https://hrms-backend.azurewebsites.net';
-const apiUrl = 'https://hrmsrender.onrender.com'
+//const apiUrl = 'http://127.0.0.1:8000';
+const apiUrl = 'https://hrmsrender.onrender.com';
 
 export default apiUrl;
